3e597af64f42457c63165f8a06a6fac2ab2ae18f
QuintsGuy <taknott@email.neit.edu>
2023-10-02 14:48:22 -0400

    finished fizzbuzz

--- Week 1/fizzbuzz.php
+++ Week 1/fizzbuzz.php
@@ -8,22 +8,27 @@
 <body>
     <h1>FizzBuzz Finder</h1>
 
+    <!--Create fizzBuzz function; Check whether numbs is divisible by 2 or 3; Print the corresponding statements-->
+    <?php function fizzBuzz ($numbs){
+        if ($numbs % 2 == 0 && $numbs % 3 ==0) {
+            echo $numbs . "FizzBuzz"."\n";
+        }
+        else if ($numbs % 2 == 0){
+            echo $numbs . "Fizz"."\n";
+        }
+        else if ($numbs % 3 == 0){
+            echo $numbs . "Buzz"."\n";
+        }
+        else {
+            echo $numbs."\n";
+        }
+    }?>
+
     <!--Loop through $numbs 100 times-->
     <table><?php for ($numbs = 1; $numbs <= 100; $numbs++) { ?>
         <tr>
-            <!--Check whether numbs is divisible by 2 or 3; Print the corresponding statements-->
-            <td><?php if ($numbs % 2 == 0 && $numbs % 3 ==0) {
-                echo $numbs . "FizzBuzz"."\n";
-            }
-            else if ($numbs % 2 == 0){
-                echo $numbs . "Fizz"."\n";
-            }
-            else if ($numbs % 3 == 0){
-                echo $numbs . "Buzz"."\n";
-            }
-            else {
-                echo $numbs."\n";
-            }?></td>
+            <!--Call fizzBuzz function using $numbs variable-->
+            <td><?php fizzBuzz($numbs)?></td>
         </tr>
     <?php } ?></table>
 
