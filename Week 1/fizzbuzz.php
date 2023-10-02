0da321d54085c8b458bf5e5e6c3681ab2f6b55bd
QuintsGuy <taknott@email.neit.edu>
2023-10-02 14:44:28 -0400

    finished fizzbuzz

--- Week 1/fizzbuzz.php
+++ Week 1/fizzbuzz.php
@@ -8,8 +8,10 @@
 <body>
     <h1>FizzBuzz Finder</h1>
 
+    <!--Loop through $numbs 100 times-->
     <table><?php for ($numbs = 1; $numbs <= 100; $numbs++) { ?>
         <tr>
+            <!--Check whether numbs is divisible by 2 or 3; Print the corresponding statements-->
             <td><?php if ($numbs % 2 == 0 && $numbs % 3 ==0) {
                 echo $numbs . "FizzBuzz"."\n";
             }
