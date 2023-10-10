b00537dc9145030cb077ee68d48ad37d6d01af5f
QuintsGuy <taknott@email.neit.edu>
2023-10-10 14:36:30 -0400

    Established W2 Landing Page

---
+++ site/index.php
@@ -0,0 +1,26 @@
+<?php include __DIR__ . '/../include/header.php' ?>
+
+
+<h1>PHP & MySQL - Tristan Knott</h1>
+
+<p>Welcome to my PHP & MySQL page. You can find an overview of all my working PHP projects along with my code.</p>
+
+<p>Original Author: Tristan Knott</p>
+
+<h2>Assignment List</h2>
+
+<ul>
+    <li><a href="Week 1/fizzbuzz.php">Week 1 - Fizz Buzz</a></li>
+    <li><a href="Week 1/index.php">Week 1 - Video Lab Assignments</a></li>
+    <li><a href="Week 2/index.php">Week 2 - Patient</a></li>
+    <li><a href="Week 3/index.php">Week 3 - ATM</a></li>
+    <li><a href="Week 4/index.php">Week 4 - Patients</a></li>
+    <li><a href="Week 5/index.php">Week 5 - Patient CRUD</a></li>
+    <li><a href="Week 6/index.php">Week 6 - Patient Metrics</a></li>
+    <li><a href="Week 6/index.php">Week 6 - Schools</a></li>
+    <li><a href="Week 8/index.php">Week 8 - Disney Votes</a></li>
+</ul>
+
+<br>
+
+<?php include __DIR__ . '/../include/footer.php' ?>
