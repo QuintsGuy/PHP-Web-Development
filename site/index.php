3fb7ad64e206b995ac8d29b7db675f8eeb852471
QuintsGuy <taknott@email.neit.edu>
2023-10-16 14:17:08 -0400

    Finished W2 Assignment

--- site/index.php
+++ site/index.php
@@ -10,15 +10,15 @@
 <h2>Assignment List</h2>
 
 <ul>
-    <li><a href="Week 1/fizzbuzz.php">Week 1 - Fizz Buzz</a></li>
-    <li><a href="Week 1/index.php">Week 1 - Video Lab Assignments</a></li>
-    <li><a href="Week 2/index.php">Week 2 - Patient</a></li>
-    <li><a href="Week 3/index.php">Week 3 - ATM</a></li>
-    <li><a href="Week 4/index.php">Week 4 - Patients</a></li>
-    <li><a href="Week 5/index.php">Week 5 - Patient CRUD</a></li>
-    <li><a href="Week 6/index.php">Week 6 - Patient Metrics</a></li>
-    <li><a href="Week 6/index.php">Week 6 - Schools</a></li>
-    <li><a href="Week 8/index.php">Week 8 - Disney Votes</a></li>
+    <li><a href="../Week 1/fizzbuzz.php">Week 1 - Fizz Buzz</a></li>
+    <li><a href="../Week 1/index.php">Week 1 - Video Lab Assignments</a></li>
+    <li><a href="../Week 2/index.php">Week 2 - Patient</a></li>
+    <li><a href="../Week 3/index.php">Week 3 - ATM</a></li>
+    <li><a href="../Week 4/index.php">Week 4 - Patients</a></li>
+    <li><a href="../Week 5/index.php">Week 5 - Patient CRUD</a></li>
+    <li><a href="../Week 6/index.php">Week 6 - Patient Metrics</a></li>
+    <li><a href="../Week 6/index.php">Week 6 - Schools</a></li>
+    <li><a href="../Week 8/index.php">Week 8 - Disney Votes</a></li>
 </ul>
 
 <br>
