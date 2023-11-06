b7112e78a3d3dca2d26908202585333e6061fc29
QuintsGuy <taknott@email.neit.edu>
2023-11-06 18:19:39 -0500

    Finished W6 Login & Search Functions

--- Week 4/patients/view_patients.php
+++ Week 4/patients/view_patients.php
@@ -14,7 +14,7 @@
         include __DIR__ . '/../../include/header.php';
 
         if(!isset($_SESSION['user'])){
-            header('Location: /../..include/restricted.php');
+            header('Location: ../..include/restricted.php');
         }
         
         $patients = getPatients();
